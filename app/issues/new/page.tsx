d3fd05f0f51ad5e9570410fc99b79be73bead643
Donatelo27 <97600974+DonBos27@users.noreply.github.com>
2023-12-18 19:49:16 +0100

    Create the ErrorMessage component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -11,6 +11,7 @@ import { GoAlert } from "react-icons/go";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssuesSchema } from "@/app/ValidationSchema";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 // interface IssueForm {
 //   title: string;
@@ -57,7 +58,7 @@ const NewIssuePage = () => {
           {/* <TextField.Slot></TextField.Slot> */}
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
-        {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           name="description"
           control={control}
@@ -65,9 +66,7 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description…" {...field} />
           )}
         />
-        {errors.description && (
-          <Text color="red" as="p">{errors.description.message}</Text>
-        )}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button>Submit New Issue</Button>
       </form>
     </div>
