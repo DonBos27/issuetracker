47f1c20f7ca37c8420eb1ebd1616249ecd84045b
Donatelo27 <97600974+DonBos27@users.noreply.github.com>
2023-12-18 19:29:40 +0100

    implement client-side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -8,15 +8,27 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { GoAlert } from "react-icons/go";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssuesSchema } from "@/app/ValidationSchema";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+// interface IssueForm {
+//   title: string;
+//   description: string;
+// }
+
+type IssueForm = z.infer<typeof createIssuesSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssuesSchema),
+  });
   console.log(register("title"));
   const [error, setError] = useState("");
 
@@ -45,6 +57,7 @@ const NewIssuePage = () => {
           {/* <TextField.Slot></TextField.Slot> */}
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
+        {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
         <Controller
           name="description"
           control={control}
@@ -52,7 +65,9 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description…" {...field} />
           )}
         />
-
+        {errors.description && (
+          <Text color="red" as="p">{errors.description.message}</Text>
+        )}
         <Button>Submit New Issue</Button>
       </form>
     </div>
